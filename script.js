6fe40ed51e27a232a044ac08a9fe9ce84d3cda41
HenriqueLeardi <hojegamepvp@gmail.com>
2022-04-15 23:08:18 -0300

    commit inicial

---
+++ script.js
@@ -0,0 +1,18 @@
+var email = document.getElementById ('email');
+    
+        email.addEventListener('focus',()=>{
+        email.style.bordercolor = "#4A5F6A";
+        });
+
+        var email = document.getElementById ('email');
+    
+        email.addEventListener('blur',()=>{
+        email.style.bordercolor = "#4A5F6A";
+        });
+
+        var password = document.getElementById ('password');
+    
+        password.addEventListener('focus',()=>{
+        password.style.bordercolor = "#4A5F6A";
+        });
+
